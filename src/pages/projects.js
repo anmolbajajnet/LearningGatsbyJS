91eca0a11f798a8ad205bc862a7f38ce4c1c2309
anmolbajajnet <hello@anmolbajaj.net>
2018-09-10 23:15:14 -0700

    First commit

---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default() => (
+    <div>
+    <h1>List all your projects here</h1>
+    <Link to="/">back home</Link>
+    </div>
+);
